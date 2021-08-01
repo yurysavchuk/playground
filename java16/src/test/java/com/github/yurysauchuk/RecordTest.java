a200e200ee00f6bac064de8caa384ad30d45a491
yurysavchuk <yury.savchuk95@gmail.com>
2021-08-01 16:08:07 +0200

    Records, Java 16

---
+++ java16/src/test/java/com/github/yurysauchuk/RecordTest.java
@@ -0,0 +1,32 @@
+package com.github.yurysauchuk;
+
+import org.junit.jupiter.api.Test;
+
+public class RecordTest {
+
+    record Point(int x, int y) {}
+
+    @Test
+    void test() {
+        Point p1 = new Point(10, 20);
+        System.out.println(p1.x());
+        System.out.println(p1.y());
+
+        Point p2 = new Point(11, 22);
+        System.out.println(p2.x());
+        System.out.println(p2.y());
+
+        Point p3 = new Point(10, 20);
+        System.out.println(p1.x());
+        System.out.println(p1.y());
+
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+        System.out.println(p3.hashCode());
+
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+        System.out.println(p2.equals(p3));
+
+    }
+}
